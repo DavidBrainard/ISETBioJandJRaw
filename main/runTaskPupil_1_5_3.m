2f3f839f40be5769fe214fe81328cecf8eb75cc1
David Brainard <brainard@psych.upenn.edu>
2024-03-19 21:11:13 -0400

    Try harder not to have the output of one script clobber that of another.

--- main/runTaskPupil_1_5_3.m
+++ main/runTaskPupil_1_5_3.m
@@ -98,6 +98,29 @@ function runTask()
         'Uniform_FullVis_LCA_3590_TCA_Hz3150_TCA_Vt6300.mat' , 3.6, 6.3 ...
         };
 
+    % Set up summary filename and output dir
+    summaryFileName = sprintf('Summary_%s_%dms.mat', strrep(params.psfDataSubDir, '.mat', ''), round(1000*params.mosaicIntegrationTimeSeconds));
+    if (~isempty(params.customMacularPigmentDensity))
+        summaryFileName = strrep(summaryFileName, '.mat', sprintf('_MPD_%2.2f.mat', params.customMacularPigmentDensity));
+    end
+    if (~isempty(params.customPupilDiameterMM))
+        summaryFileName = strrep(summaryFileName, '.mat', sprintf('_pupilDiamMM_%2.2f.mat', params.customPupilDiameterMM));
+    end
+    if (~isempty(params.customConeDensities))
+        summaryFileName = strrep(summaryFileName, '.mat', sprintf('_cones_%2.2f_%2.2f_%2.2f.mat', params.customConeDensities(1), params.customConeDensities(2), params.customConeDensities(3)));
+    end
+    if (~isempty(params.customLensAgeYears))
+        summaryFileName = strrep(summaryFileName, '.mat', sprintf('_lensAge_%d.mat', params.customLensAgeYears));
+    end
+    params.outputResultsDir = fullfile(ISETBioJandJRootPath,'results',strrep(summaryFileName, '.mat',''));
+    params.outputFiguresDir =  fullfile(ISETBioJandJRootPath,'figures',strrep(summaryFileName, '.mat',''));
+    if (~exist(params.outputResultsDir,'dir'))
+        mkdir(params.outputResultsDir);
+    end
+    if (~exist(params.outputFiguresDir,'dir'))
+        mkdir(params.outputFiguresDir);
+    end
+
     % Loop over all the specified PSFs.  This loop saves the data out for
     % each PSF, as well as accumulates the threshold for each.
     for iPSF = 1:size(examinedPSFDataFiles,1)
@@ -112,20 +135,7 @@ function runTask()
 
     % Save summary,  This allows examination of the numbers and/or
     % replotting.
-    summaryFileName = sprintf('Summary_%s.mat', strrep(params.psfDataSubDir, '.mat', ''));
-    if (~isempty(params.customMacularPigmentDensity))
-        summaryFileName = strrep(summaryFileName, '.mat', sprintf('_MPD_%2.2f.mat', params.customMacularPigmentDensity));
-    end
-    if (~isempty(params.customPupilDiameterMM))
-        summaryFileName = strrep(summaryFileName, '.mat', sprintf('_pupilDiamMM_%2.2f.mat', params.customPupilDiameterMM));
-    end
-    if (~isempty(params.customConeDensities))
-        summaryFileName = strrep(summaryFileName, '.mat', sprintf('_cones_%2.2f_%2.2f_%2.2f.mat', params.customConeDensities(1), params.customConeDensities(2), params.customConeDensities(3)));
-    end
-    if (~isempty(params.customLensAgeYears))
-        summaryFileName = strrep(summaryFileName, '.mat', sprintf('_lensAge_%d.mat', params.customLensAgeYears));
-    end
-    save(fullfile(ISETBioJandJRootPath,'results',summaryFileName),"examinedPSFDataFiles","threshold","logMAR","LCA","TCA","theConeMosaic");
+    save(fullfile(params.outputResultsDir,summaryFileName),"examinedPSFDataFiles","threshold","logMAR","LCA","TCA","theConeMosaic");
     
     % Make and save a figure of what happened. This is not publication
     % quality, but does match up with the key figure in the paper in tersm
@@ -165,168 +175,6 @@ function runTask()
     legend(legendStr);
     titleStr = LiteralUnderscore(strrep(summaryFileName,'.mat',''));
     title(titleStr);
-    NicePlot.exportFigToPDF(strrep(fullfile(ISETBioJandJRootPath,'figures',summaryFileName),'.mat','.pdf'),summaryFig,300);
-end
-
-function [theConeMosaic,threshold] = runSimulation(params, theConeMosaic)
-
-    % Unpack simulation params
-    letterSizesNumExamined = params.letterSizesNumExamined;
-    maxLetterSizeDegs = params.maxLetterSizeDegs;
-    mosaicIntegrationTimeSeconds = params.mosaicIntegrationTimeSeconds;
-    nTest = params.nTest;
-    thresholdP = params.thresholdP;
-    spdDataFile = params.spdDataFile;
-    psfDataFile = fullfile(params.psfDataSubDir, params.psfDataFile);
-    
-    % Generate optics from custom PSFs
-    theCustomPSFOptics = generateCustomOptics(psfDataFile, params.customPupilDiameterMM, params.customLensAgeYears);
-
-    % Visualization of the PSF stack
-    if (~isempty(params.visualizedPSFwavelengths))
-        psfRangeArcMin = 10;
-        visualizePSFstack(theCustomPSFOptics, params.visualizedPSFwavelengths, psfRangeArcMin)
-    end
-    
-    if (isempty(theConeMosaic))
-        % Generate cone mosaic to use
-        mosaicSizeDegs = maxLetterSizeDegs*1.25*[1 1];
-        theConeMosaic = generateCustomConeMosaic(...
-            mosaicIntegrationTimeSeconds, ...
-            theCustomPSFOptics, ...
-            mosaicSizeDegs, ...
-            'customMPD', params.customMacularPigmentDensity, ...
-            'customConeDensities', params.customConeDensities);
-    end
-
-    %% Create neural response engine
-    %
-    % This calculations isomerizations in a patch of cone mosaic with Poisson
-    % noise, and includes optical blur.
-    neuralResponsEngineComputeFunction = @nrePhotopigmentExcitationsCmosaicSingleShot;
-    neuralParams = neuralResponsEngineComputeFunction();
-
-    % Instantiate the neural engine with the default params
-    theNeuralEngine = neuralResponseEngine(neuralResponsEngineComputeFunction, neuralParams);
-
-    % Update the neural pipeline with custom cone mosaic and custom PSF - based optics
-    theNeuralEngine.customNeuralPipeline(struct(...
-              'coneMosaic', theConeMosaic, ...
-              'optics', theCustomPSFOptics));
-
-    % Poisson n-way AFC
-    classifierEngine = responseClassifierEngineNWay(@rcePoissonNWay_OneStimPerTrial);
-
-    % Parameters associated with use of the Poisson classifier.
-    classifierPara = struct('trainFlag', 'none', ...
-                            'testFlag', 'random', ...
-                            'nTrain', 1, 'nTest', nTest);
-
-    % Tumbling E setup
-    orientations = [0 90 180 270];
-    
-    %% Parameters for threshold estimation/quest engine
-    thresholdParameters = struct(...
-        'maxParamValue', maxLetterSizeDegs, ...    % The maximum value of the examined param (letter size in degs)
-        'logThreshLimitLow', 2.0, ...              % minimum log10(normalized param value)
-        'logThreshLimitHigh', 0.0, ...             % maximum log10(normalized param value)
-        'logThreshLimitDelta', 0.01, ...
-        'slopeRangeLow', 1/20, ...
-        'slopeRangeHigh', 500/20, ...
-        'slopeDelta', 2/20, ...
-        'thresholdCriterion', thresholdP, ...
-        'guessRate', 1/numel(orientations), ...
-        'lapseRate', [0 0.02]);
-
-    
-    % Parameters for Quest
-    questEnginePara = struct( ...
-        'qpPF',@qpPFWeibullLog, ...
-        'minTrial', nTest*letterSizesNumExamined, ...
-        'maxTrial', nTest*letterSizesNumExamined, ...
-        'numEstimator', 1, ...
-        'stopCriterion', 0.05);
-
-    % Generate a customSceneParams struct from the defaultSceneParams
-    % so we can set certain scene params of interest
-    theSceneEngine = createTumblingEsceneEngine(0);
-    customSceneParams = theSceneEngine.sceneComputeFunction();
-    customSceneParams.yPixelsNumMargin = 100;
-    customSceneParams.xPixelsNumMargin = 100;
-
-    % Change upsample factor if we want smaller pixels
-    customSceneParams.upSampleFactor = uint8(params.sceneUpSampleFactor);
-       
-    % Set the spdDataFile
-    customSceneParams.spdDataFile = spdDataFile;
-        
-    if (params.visualizeDisplayCharacteristics)
-        visualizationSceneParams = customSceneParams;
-        visualizationSceneParams.plotDisplayCharacteristics = true;
-        theSceneEngine.sceneComputeFunction(theSceneEngine,0.3, visualizationSceneParams);
-    end
-    
-    if (params.visualizeScene)
-        visualizationSceneParams = customSceneParams;
-        visualizationSceneParams.visualizeScene = true;
-        theSceneEngine.sceneComputeFunction(theSceneEngine,0.3, visualizationSceneParams);
-    end 
-    clear 'theSceneEngine';
-    
-    % Generate scene engines for the tumbling E's (4 orientations)
-    tumblingEsceneEngines = cell(1,numel(orientations));
-    for iOri = 1:numel(orientations)
-       tumblingEsceneEngines{iOri} = createTumblingEsceneEngine(...
-           orientations(iOri), ...
-           'customSceneParams', customSceneParams);
-    end
-
-    % Generate background scene engine.params for the background scene
-    sceneParams = tumblingEsceneEngines{1}.sceneComputeFunction();
-    backgroundSceneParams = sceneParams;
-    backgroundSceneParams.chromaSpecification.foregroundRGB = sceneParams.chromaSpecification.backgroundRGB;
-    backgroundSceneEngine = createTumblingEsceneEngine(orientations(1), 'customSceneParams', backgroundSceneParams);
-
-    % Compute psychometric function for the 4AFC paradigm with the 4 E scenes
-    [threshold, questObj, psychometricFunction, fittedPsychometricParams] = computeParameterThreshold(...
-            tumblingEsceneEngines, theNeuralEngine, classifierEngine, ...
-            classifierPara, thresholdParameters, questEnginePara, ...
-            'visualizeAllComponents', ~true, ...
-            'beVerbose', true);
-
-    % Plot the derived psychometric function and other things.  The lower
-    % level routines put this in ISETBioJandJRootPath/figures.
-    pdfFileName = sprintf('Performance_%s_Reps_%d.pdf', strrep(params.psfDataFile, '.mat', ''), nTest);
-    plotDerivedPsychometricFunction(questObj, threshold, fittedPsychometricParams, ...ISETBio
-        thresholdParameters, pdfFileName, 'xRange', [0.02 0.2]);  
-    if (params.visualEsOnMosaic)
-        pdfFileName = sprintf('Simulation_%s_Reps_%d.pdf', strrep(params.psfDataFile, '.mat', ''), nTest);
-        visualizeSimulationResults(questObj, threshold, fittedPsychometricParams, ...
-            thresholdParameters, tumblingEsceneEngines, theNeuralEngine, pdfFileName);
-    end
-
-    % Export the results
-    exportFileName = sprintf('Results_%s_Reps_%d.mat', strrep(params.psfDataFile, '.mat', ''), nTest);
-    if (~isempty(params.customMacularPigmentDensity))
-        exportFileName = strrep(exportFileName, '.mat', sprintf('_MPD_%2.2f.mat', params.customMacularPigmentDensity));
-    end
-    if (~isempty(params.customPupilDiameterMM))
-        exportFileName = strrep(exportFileName, '.mat', sprintf('_PupilDiamMM_%2.2f.mat', params.customPupilDiameterMM));
-    end
-    if (~isempty(params.customConeDensities))
-        exportFileName = strrep(exportFileName, '.mat', sprintf('_cones_%2.2f_%2.2f_%2.2f.mat', params.customConeDensities(1), params.customConeDensities(2), params.customConeDensities(3)));
-    end
-    if (~isempty(params.customLensAgeYears))
-        exportFileName = strrep(exportFileName, '.mat', sprintf('_lensAge_%d.mat', params.customLensAgeYears));
-    end
-
-    fprintf('Saving data to %s\n', fullfile(ISETBioJandJRootPath,'results',exportFileName));
-    exportSimulation(questObj, threshold, fittedPsychometricParams, ...
-        thresholdParameters, classifierPara, questEnginePara, ...
-        tumblingEsceneEngines, theNeuralEngine, classifierEngine, ...
-        fullfile(ISETBioJandJRootPath,'results',exportFileName));
-
-    % Append the params struct
-    save(fullfile(ISETBioJandJRootPath,'results',exportFileName), 'params', '-append');
+    NicePlot.exportFigToPDF(fullfile(params.outputFiguresDir,strrep(summaryFileName,'.mat','.pdf')),summaryFig,300);
 end
 
