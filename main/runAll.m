9fa3c84b2f68568302ac1a22675f51cbcc2686c2
David Brainard <brainard@psych.upenn.edu>
2024-03-21 12:57:47 -0400

    More pupil sizes, just for fun

--- main/runAll.m
+++ main/runAll.m
@@ -1,12 +1,15 @@
-%runTaskPaper
-%runTaskLens_60
-%runTaskMac_0_70
-%runTaskPupil_1_5
-runTaskPupil_2_5
-%runTaskPupil_4_0
-%runTaskLOnly
-%runTaskMOnly
-%runTaskInt_100ms
-runTaskPupil_1_5_4
-runTaskPupil_2_5_4
-runTaskPupil_4_0_4
+% runTaskPaper
+% runTaskLens_60
+% runTaskMac_0_70
+% runTaskPupil_1_5
+runTaskPupil_2_0
+% runTaskPupil_2_5
+runTaskPupil_3_0
+runTaskPupil_3_5
+% runTaskPupil_4_0
+% runTaskLOnly
+% runTaskMOnly
+% runTaskInt_100ms
+% runTaskPupil_1_5_4
+% runTaskPupil_2_5_4
+% runTaskPupil_4_0_4
